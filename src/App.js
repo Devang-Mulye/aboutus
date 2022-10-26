fcf7c43013c8f8e9147a9cadde8a3a881eced4bb
Devang <devangmulye@gmail.com>
2022-10-26 19:19:40 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import img1 from "./images/img1.jpg";
+import newton from "./images/newton.jpg"
+import map from "./images/map.jpeg"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="about">
+      <div className = "aboutushead">
+        <h1>About US</h1>
+      </div>
+
+     <div className="proimage">
+        <a href="www.google.com"> <img src = {img1} alt = "Bike" /> </a>
+     </div>
+
+     <div className="companydetails">
+       <h2>Blah blah info about company and other stuff</h2>
+       <h2>Aur 1-2 lines add karde aman yr mai karta hoon</h2>
+       <h2>Like description likh sakte idhar</h2>
+     </div>
+
+     <div className="founderinfo">
+      <div className="founderimg">
+        <a href="https://en.wikipedia.org/wiki/Isaac_Newton"> <img id = "newton" src= {newton} alt="Founder" /></a>
+      </div>
+      <div className="foundertext">
+        <h2>Some info about the founder and why he has opened this company</h2>
+      </div>
+     </div>
+
+     <div className="placeinfo">
+      <div className="mapimg">
+        <a href="https://www.google.com/maps/place/SIES+parking/@19.0424385,73.0219043,17.25z/data=!4m5!3m4!1s0x3be7c32c3126c233:0x8b9750bd172bf90!8m2!3d19.0426804!4d73.0226144"> <img id = "map" src= {map} alt="Map" /></a>
+      </div>
+      <div className="showrooms">
+        <h2>Mumbai showroom</h2>
+        <h3>Click on the map for precise directions to our nearest dealership</h3>
+    
+        
+      </div>
+     </div>
     </div>
   );
 }
-
-export default App;
